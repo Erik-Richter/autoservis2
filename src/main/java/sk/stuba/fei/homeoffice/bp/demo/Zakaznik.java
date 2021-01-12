a154220137975997ea236aaa9df01ab529f3c75c
erik-ctrl <richtererik20@gmail.com>
2021-01-12 13:13:54 +0100

    added new dependencies db communication still not working

--- src/main/java/sk/stuba/fei/homeoffice/bp/demo/Zakaznik.java
+++ src/main/java/sk/stuba/fei/homeoffice/bp/demo/Zakaznik.java
@@ -4,11 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "tabulkaZakaznikov")
 public class Zakaznik {
 
+    @Id
+    @Column(name = "idenetifikator")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int idenetifikator;
+    @Column(name = "meno")
     private String meno;
 }
--- src/main/java/sk/stuba/fei/homeoffice/bp/demo/ZakaznikController.java
+++ src/main/java/sk/stuba/fei/homeoffice/bp/demo/ZakaznikController.java
@@ -1,6 +1,8 @@
 package sk.stuba.fei.homeoffice.bp.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
